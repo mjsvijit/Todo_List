8513072f443ff21633e292598558588b2bb58399
mjsvijit <viratsinghfzd75@gmail.com>
2022-07-18 20:56:16 +0530

    third update

--- src/Components/Pages/Navbar.jsx
+++ src/Components/Pages/Navbar.jsx
@@ -4,12 +4,14 @@ import styles from "./Navbar.module.css"
 
 const Navbar = () => {
   return (
+    <div> 
+    <br/>
     <div className={styles.maindiv}>
      <Link className={styles.left} to="">ToDoList</Link>
      <Link  className={styles.right} to='/undone'>Undone</Link>
      <Link  className={styles.right} to='/done'>Done</Link>
      <Link  className={styles.right} to='/'>All</Link>
-     
+     </div>
    
     
     </div>
